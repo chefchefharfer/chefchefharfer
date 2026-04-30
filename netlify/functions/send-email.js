0cae3e3257a0414301e3fa32a31b0470efeac3d7
chefchefharfer <haro.osw@gmail.com>
2026-04-29 23:30:00 -0600

    Update send-email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -3,8 +3,7 @@
 // Maneja 3 tipos de correo: bienvenida, recuperacion, confirmacion
 
 const RESEND_API = 'https://api.resend.com/emails';
-// Temporal: usar dominio de prueba de Resend mientras se verifica chefchefharfer.mx
-const FROM = 'Chef Chef Harfer <onboarding@resend.dev>';
+const FROM = 'Chef Chef Harfer <hola@chefchefharfer.mx>';
 
 // Plantillas de correo
 function templateBienvenida(nombre) {
